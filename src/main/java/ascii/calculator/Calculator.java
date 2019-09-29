358f08026ba23e0dff275e8bfec4d64f15b8661e
stea1th <stea1th@mail.ru>
2019-09-29 13:36:02 +0200

    add junit5 to pom

--- src/main/java/ascii/calculator/Calculator.java
+++ src/main/java/ascii/calculator/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
 
     public static void main(String[] args) {
 //        String argument = args[0];
-        String argument = "2 - 3";
+        String argument = "-1+2 - 3+ 2 -3 ";
         Integer computed = MathHelper.compute(StringHelper.cutStringToPairs(argument));
         argument += " = " + computed;
         SignHelper.printResult(SignHelper.transformToList(argument));
