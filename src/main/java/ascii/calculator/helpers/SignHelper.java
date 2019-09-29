078c8b5b62356208b78d3b2c389ff17c10733078
stea1th <stea1th@mail.ru>
2019-09-29 20:01:53 +0200

    fix helper

--- src/main/java/ascii/calculator/helpers/SignHelper.java
+++ src/main/java/ascii/calculator/helpers/SignHelper.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class SignHelper {
 
-    public SignHelper() {
+    private SignHelper() {
         throw new CalculatorException("Utility Class");
     }
 
